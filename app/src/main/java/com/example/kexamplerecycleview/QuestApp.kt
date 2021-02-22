1be4b0c56fa50d385dc16a37b6caf2342555a50e
koassumo <0770407@gmail.com>
2021-02-22 21:05:26 +0300

    инит интернет 2

--- app/src/main/java/com/example/kexamplerecycleview/QuestApp.kt
+++ app/src/main/java/com/example/kexamplerecycleview/QuestApp.kt
@@ -3,7 +3,7 @@ package com.example.kexamplerecycleview
 import android.app.Application
 import com.example.kexamplerecycleview.model.apiData.QuestApi
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingIntercepter
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,15 +20,15 @@ class QuestApp: Application() {
     }
 
     private fun configureRetrofit() {
-        val httpLoggingIntercepter = HttpLoggingIntercepter()
-        httpLoggingIntercepter.level = HttpLoggingIntercepter.Level.BODY
+        val httpLoggingIntercepter = HttpLoggingInterceptor()
+        httpLoggingIntercepter.level = HttpLoggingInterceptor.Level.BODY
 
         val okHttpClient = OkHttpClient.Builder()
             .addInterceptor(httpLoggingIntercepter)
             .build()
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://pokeapi.co/api/v2")
+            .baseUrl("https://api.github.com/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
